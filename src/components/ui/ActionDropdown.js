cd58933f681f2ec27968cb69b276f0f9bffb8149
Imammulhaq <imam_chelsea2@apps.ipb.ac.id>
2021-06-17 15:41:47 +0700

    get data fixed

--- src/components/ui/ActionDropdown.js
+++ src/components/ui/ActionDropdown.js
@@ -2,6 +2,18 @@ import { DropdownButton, Dropdown } from "react-bootstrap";
 import BsThreeDots from "module";
 
 function ActionDropdown(props) {
+  function handleAccepted() {
+    console.log("diterima");
+  }
+
+  function handleRejected() {
+    console.log("rejected");
+  }
+
+  function handleDelete() {
+    console.log("Delete");
+  }
+
   return (
     <DropdownButton
       size="sm"
@@ -9,9 +21,15 @@ function ActionDropdown(props) {
       id="dropdown-basic-button"
       title=""
     >
-      <Dropdown.Item href="#/action-1">{props.aksi1}</Dropdown.Item>
-      <Dropdown.Item href="#/action-2">{props.aksi2}</Dropdown.Item>
-      <Dropdown.Item href="#/action-3">{props.aksi3}</Dropdown.Item>
+      <Dropdown.Item href="#/action-1" onClick={handleAccepted}>
+        {props.aksi1}
+      </Dropdown.Item>
+      <Dropdown.Item href="#/action-2" onClick={handleRejected}>
+        {props.aksi2}
+      </Dropdown.Item>
+      <Dropdown.Item href="#/action-3" onClick={handleDelete}>
+        {props.aksi3}
+      </Dropdown.Item>
     </DropdownButton>
   );
 }
--- src/components/ui/MyNasionalTable.js
+++ src/components/ui/MyNasionalTable.js
@@ -8,32 +8,20 @@ import axios from "axios";
 import react, { useState, useEffect, useContext, UserContext } from "react";
 
 function MyTable() {
-  const [pengajuan, setPengajuan] = useState(null);
-  const [user] = useContext(UserContext);
+  const [pengajuan, setPengajuan] = useState([]);
+
+  // const [user] = useContext(UserContext);
 
   useEffect(() => {
-    if (pengajuan == null) {
-      axios
-        .get(`http://localhost:4000/api/employee`, {
-          headers: { Authorization: `Bearer ${user.token}` },
-        })
-        .then((res) => {
-          setPengajuan(res.data.response);
-        });
-    }
-  }, [pengajuan, setPengajuan]);
-
-  // const [employees, setEmployees] = useState([]);
-  // useEffect(() => {
-  //   axios
-  //     .get("http://localhost:4000/api/employee/")
-  //     .then((res) => {
-  //       console.log(res);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // });
+    axios
+      .get("http://localhost:4000/api/employee")
+      .then((res) => {
+        setPengajuan(res.data.response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <Table striped bordered hover responsive className="mt-4">
@@ -52,79 +40,70 @@ function MyTable() {
         </tr>
       </thead>
       <tbody>
-        {pengajuan !== null &&
-          pengajuan.map((data) => {
-            return (
-              <tr>
-                <td>1</td>
-                <td>
-                  imam{" "}
-                  <ModalKetua
-                    namaKetua="imam"
-                    nim="G64180047"
-                    fakultas="Fakultas Matematika dan Ilmu Pengetahuan Alam"
-                    departemen="Ilmu Komputer"
-                    noHp="087818407648"
-                    noRek="7147678856"
-                    bank="syariah"
-                  />
-                </td>
-                <td>26/05/2021, 11:59 AM</td>
-                <td>7147678856 (Mandiri Syariah)</td>
-                <td>
-                  <ModalAnggota
-                    namaAnggota1="Faisal Wicak"
-                    nimAnggota1="G64180067"
-                    noHpAnggota1="087654293746"
-                    namaAnggota2="Bima Aulia"
-                    nimAnggota2="G64180054"
-                    noHpAnggota2="087647389187"
-                  />
-                </td>
-                <td>
-                  {data.namaKegiatan}{" "}
-                  <ModalKegiatan
-                    namaKegiatan={data.namaKegiatan}
-                    jenisKegiatan={data.jenisLomba}
-                    skala={data.skalaKegiatan}
-                    penyelenggara={data.penyelenggara}
-                    jumlahTim={data.jumlahTim}
-                    jumlahPt={data.jumlahUniv}
-                    tglKegiatan={data.tanggalPelaksanaan}
-                    urlKegiatan={data.linkWeb}
-                    urlMedsos={data.linkMedsos}
-                  />
-                </td>
-                <td>{data.prestasi}</td>
-                <td>
-                  <ModalBukti
-                    buktiSertifikat={data.sertifikat}
-                    buktiFoto={data.foto}
-                    buktiLoa={data.loa}
-                    buktiSuratIzin={data.suratIzin}
-                  />
-                </td>
-                <td>
-                  <div class="badge bg-secondary text-light">Pending</div>
-                </td>
-                <td>
-                  <ActionDropdown aksi1="Diterima" aksi2="Ditolak" />
-                </td>
-              </tr>
-            );
-          })}
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>lorem ipsum</td>
-          <td>lorem ipsum</td>
-          <td>lorem ipsum</td>
-          <td>lorem ipsum</td>
-          <td>lorem ipsum</td>
-          <td>lorem ipsum</td>
-          <td>lorem ipsum</td>
-        </tr>
+        {pengajuan.map((data) => {
+          return (
+            <tr>
+              <td>1</td>
+              <td>
+                imam{" "}
+                <ModalKetua
+                  namaKetua="imam"
+                  nim="G64180047"
+                  fakultas="Fakultas Matematika dan Ilmu Pengetahuan Alam"
+                  departemen="Ilmu Komputer"
+                  noHp="087818407648"
+                  noRek="7147678856"
+                  bank="syariah"
+                />
+              </td>
+              <td>26/05/2021, 11:59 AM</td>
+              <td>7147678856 (Mandiri Syariah)</td>
+              <td>
+                <ModalAnggota
+                  namaAnggota1={data.anggotaTim1}
+                  nimAnggota1={data.nim1}
+                  noHpAnggota1={data.phone1}
+                  namaAnggota2={data.anggotaTim2}
+                  nimAnggota2={data.nim2}
+                  noHpAnggota2={data.phone2}
+                />
+              </td>
+              <td>
+                {data.namaKegiatan}{" "}
+                <ModalKegiatan
+                  namaKegiatan={data.namaKegiatan}
+                  jenisKegiatan={data.jenisLomba}
+                  skala={data.skalaKegiatan}
+                  penyelenggara={data.penyelenggara}
+                  jumlahTim={data.jumlahTim}
+                  jumlahPt={data.jumlahUniv}
+                  tglKegiatan={data.tanggalPelaksanaan}
+                  urlKegiatan={data.linkWeb}
+                  urlMedsos={data.linkMedsos}
+                />
+              </td>
+              <td>{data.prestasi}</td>
+              <td>
+                <ModalBukti
+                  buktiSertifikat={data.sertifikat}
+                  buktiFoto={data.foto}
+                  buktiLoa={data.loa}
+                  buktiSuratIzin={data.suratIzin}
+                />
+              </td>
+              <td>
+                <div class="badge bg-secondary text-light">{data.status}</div>
+              </td>
+              <td>
+                <ActionDropdown
+                  aksi1="Diterima"
+                  aksi2="Ditolak"
+                  aksi3="Hapus Data"
+                />
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </Table>
   );
